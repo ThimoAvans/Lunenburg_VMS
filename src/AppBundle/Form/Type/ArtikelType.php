841443ca3ed9b7720d5533e4c7c4f1607def1d7c
ThimoAvans <38351358+ThimoAvans@users.noreply.github.com>
2018-05-23 10:36:29 +0200

    buttons

--- src/AppBundle/Form/Type/ArtikelType.php
+++ src/AppBundle/Form/Type/ArtikelType.php
@@ -5,7 +5,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-// use AppBundle\Controller\DefaultController;
 use AppBundle\Controller\ArtikelController;
 use AppBundle\Entity\Artikel;
 
