1a2741fcd2a0572bd92e9d0d99c3f8d7d35dc9da
ThimoAvans <38351358+ThimoAvans@users.noreply.github.com>
2018-05-24 15:20:26 +0200

    Naam artikel toegevoegd
    - euroteken toegevoegd in twig en artikeltype
    
    - naam en bijbehorende functies toegevoegd in entiteit artikel
    
    - lengtes van enkele velden aangepast
    
    - volgorde in form en overzicht aangepast
    
    - enkele comments weggehaald

--- src/AppBundle/Form/Type/ArtikelType.php
+++ src/AppBundle/Form/Type/ArtikelType.php
@@ -11,6 +11,7 @@ use AppBundle\Entity\Artikel;
 //vul aan als je andere invoerveld-typen wilt gebruiken in je formulier
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
@@ -23,73 +24,33 @@ class ArtikelType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        // $builder
-        // ->add('id', IntegerType::class) 
-        // ;
-        // $builder
-        // ->add('artikelnummer', IntegerType::class) 
-        // ;
-        // $builder
-        // ->add('magazijnlocatie', EntityType::class, array(
-        // 'class' => 'AppBundle:Artikel',
-        // 'choice_label' => 'magazijnlocatie'))
-        // ;
         $builder
-         ->add('magazijnlocatie', TextType::class)
-         ;        
-         $builder
-         ->add('technischeSpecificaties', TextType::class)
-         ;        
+         ->add('naam', TextType::class)
+         ; 
          $builder
          ->add('omschrijving', TextType::class)
-         ;       
+         ;
          $builder
-        ->add('inkoopprijs', IntegerType::class) 
-        ;         
+         ->add('technischeSpecificaties', TextType::class)
+         ; 
+        $builder
+         ->add('magazijnlocatie', TextType::class)
+         ;  
         $builder
          ->add('vervangendArtikel', TextType::class)
-         ;         
-         $builder
-        ->add('minimumVoorraad', IntegerType::class) 
+         ;                     
+        $builder
+         ->add('inkoopprijs', MoneyType::class) 
+        ;              
+        $builder
+         ->add('minimumVoorraad', IntegerType::class) 
         ;       
-         $builder
-        ->add('huidigeVoorraad', IntegerType::class) 
+        $builder
+         ->add('huidigeVoorraad', IntegerType::class) 
         ;          
-        //$builder
-        //->add('bestelserie', IntegerType::class) 
-        //;
-//         $builder->add('magazijnlocatie', ChoiceType::class, array(
-//     'choices' => array(
-//         '1' => 'en',
-//         '2' => 'es',
-//         '3'   => 'muppets',
-//         '4' => 'arr',
-//     )
-// ));
-//         $builder->add('gegevens wissen', ResetType::class, array(
-//     'attr' => array('class' => 'save'),
-// ));
-
-        //         $builder
-        //     ->add('magazijnlocatie', ChoiceType::class, [
-        //         'choices' => array([
-        // '1' => 'en',
-        // '2' => 'es',
-        // '3'   => 'muppets',
-        // '4' => 'arr',
-        //         ]),
-        //         'expanded'  => true,
-        //         'multiple'  => true,
-        //     ])
-        //     ->add('save', SubmitType::class)
-        // ;
-
-
-		//zie
-		//http://symfony.com/doc/current/forms.html#built-in-field-types
-		//voor meer typen invoer
     }
 	
+
 	public function configureOptions(OptionsResolver $resolver)
 	{
 		$resolver->setDefaults(array(
